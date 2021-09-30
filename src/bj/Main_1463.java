84accb5622925230aeff94f40c5db849341458f0
박현진 <jinnydiary@naver.com>
2021-09-30 17:46:41 +0900

    2021년 9월 30일

--- src/bj/Main_1463.java
+++ src/bj/Main_1463.java
@@ -1,25 +1,29 @@
 package bj;
 
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
+//백준 1463번 1로 만들기 - DP사용(동적계획법)(0930)
 public class Main_1463 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		
-		System.out.print(findMin(N, 0));
+		System.out.println(findMin(N, new int[N+1]));
 	}
 
-	private static int findMin(int n, int cnt) {
-		System.out.print(n + " ");
-		if(n == 1) return cnt;
-		else if(n % 3 == 0) return findMin(n/3, cnt+1);
-		else if(n % 2 == 0 && ((n-1)/3)%2 != 0 ) return findMin(n/2, cnt+1);
-		else return findMin(n-1, cnt+1);	
+	private static int findMin(int n, int[] D) {
 		
+		for(int i=2; i<=n; i++) {
+			int min = Integer.MAX_VALUE;				
+			if(i%3==0) min = Math.min(min, D[i/3]+1);
+			if(i%2==0) min = Math.min(min, D[i/2]+1);
+			if(i-1>=1) min = Math.min(min, D[i-1]+1);
+			D[i] = min;
+		}
+			
+		return D[n];
 	}
 
 }
