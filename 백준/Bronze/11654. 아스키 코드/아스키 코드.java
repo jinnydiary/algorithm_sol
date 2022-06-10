7a945a0df9cc88b7234a9d10ef4e5c37a2a10d46
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-11 01:35:51 +0900

    Update 아스키코드.java
    이전 코드 업로드됨

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		Scanner scr = new Scanner(System.in);
-		char word = scr.nextLine().charAt(0);
-		System.out.println((int)word);
+		System.out.print((int)scr.next().charAt(0));
 		scr.close();
 	}
 
