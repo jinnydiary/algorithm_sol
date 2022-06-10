7275d1d480b5ed69d08d5e46387feb6fe810a44d
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-11 00:48:05 +0900

    [Bronze V] Title: 윤년, Time: 108 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scr = new Scanner(System.in);
+		int year = scr.nextInt();
+		int result;
+		
+		if( ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) ) {
+			result = 1;
+		}else {
+			result = 0;
+		}
+		System.out.print(result);
+		scr.close();
+	}
+
+}
