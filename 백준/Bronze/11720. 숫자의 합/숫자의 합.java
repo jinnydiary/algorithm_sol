0b89573884b4ae7118f9f2144a8b8abac9cdb187
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-11 01:30:56 +0900

    Update 숫자의합.java - 이전 코드 업로드됨

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,16 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner scr = new Scanner(System.in);
-		int n1 = scr.nextInt();
-		String num = scr.next();
+	public static void main(String[] args) throws IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		char[] ch = br.readLine().toCharArray();
 		int sum = 0;
-		for(int i=0; i<n1; i++)
-			sum += Integer.parseInt(num.substring(i,i+1));
-		System.out.println(sum);
-		scr.close();
+		for(int n = 0; n < N; ++n)
+			sum += ch[n] - '0';
+		System.out.print(sum);
 	}
 
 }
