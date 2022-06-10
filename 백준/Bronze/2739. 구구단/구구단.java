258a8e00394c258e314f82db0840d18088e2416a
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-10 17:05:12 +0900

    Update 구구단.java - 이전 코드 업로드됨

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,17 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner scr = new Scanner(System.in);
-		int n = scr.nextInt();
-		for(int i=1; i<=9; i++) {
-			if(i < 9)
-				System.out.println(n + " * " + i + " = " + n*i);
-			else
-				System.out.print(n + " * " + i + " = " + n*i);
-		}
-		scr.close();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 1; i <= 9; ++i)
+			System.out.println(N + " * " + i + " = " + N*i);
 
 	}
 
