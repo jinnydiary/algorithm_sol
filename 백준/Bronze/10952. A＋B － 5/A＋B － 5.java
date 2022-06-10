fe5f883d104496c370800b68bece3518f75a1441
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-11 01:44:37 +0900

    Update A＋B － 5.java
    이전 코드 업로드됨

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -4,14 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scr = new Scanner(System.in);
-		int a, b;
+		StringBuilder sb = new StringBuilder();
 		while(true) {
-			a = scr.nextInt();
-			b = scr.nextInt();
-			if( (a == 0) && (b == 0))
-				break;
-			System.out.println(a+b);
+			int a = scr.nextInt();
+			int b = scr.nextInt();
+			if(a == 0 && b == 0) break;
+			sb.append(a+b + "\n");
 		}
+		System.out.print(sb);
 		scr.close();
 	}
 
