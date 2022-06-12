01018bc59c6661ce80512c3ceee8094bb26217c3
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-12 20:03:02 +0900

    Update OX퀴즈.java
    이전 코드 업로드됨

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,25 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner scr = new Scanner(System.in);
-		int t = scr.nextInt();
-		for(int i = 0; i < t; ++i) {
-			String str = scr.next();
-			int sum = 0, point = 0;
-			for(int j = 0; j < str.length(); ++j) {
-				if(str.charAt(j) == 'X')
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 0; t < T; ++t) {
+			char[] ch = br.readLine().toCharArray();
+			int total = 0, score = 0;
+			for(int i = 0; i < ch.length; ++i) {
+				if(ch[i] == 'X') {
+					score = 0;
 					continue;
-				else if(j == 0 || str.charAt(j-1) == 'X')
-					point = 1;
-				else
-					++point;
-                
-				sum += point;
+				}
+				total += ++score;
 			}
-			System.out.println(sum);
+			sb.append(total + "\n");
 		}
-		scr.close();
+		
+		System.out.print(sb);
 	}
+
 }
