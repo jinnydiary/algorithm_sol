ce1ea4a8afa9b9df7ce625494f61d3c164d4c21c
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-12 19:57:00 +0900

    [Bronze II] Title: 나머지, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scr  = new Scanner(System.in);
+		int[] num = new int[10];
+		int[] cnt = new int[42];
+		for(int i = 0; i < num.length; ++i) {
+			num[i] = scr.nextInt();
+			++cnt[num[i] % 42];
+		}
+		
+		int count = 0;
+		for(int i = 0; i < cnt.length; ++i) {
+			if(cnt[i] == 0)
+				continue;
+			else if(cnt[i] > 0)
+				++count;
+		}
+		System.out.print(count);
+		scr.close();
+	}
+
+}
