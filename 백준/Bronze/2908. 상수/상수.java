54f4c2d2fc97adbe8e301c9f6fae470b5833373d
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-12 20:40:36 +0900

    Update 상수.java
    이전 코드 업로드됨

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -4,18 +4,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scr = new Scanner(System.in);
-		int n1 = scr.nextInt(), n2 = scr.nextInt();
-		int num = 0, t1 = n1, t2 = n2;
-		for(int i=1; i<3; i++) {
-			if( (t1 % 10) != (t2 % 10) ) {
-				num = ((t1 % 10) > (t2 % 10)) ? n1 : n2;
+		int first = scr.nextInt();
+		int second = scr.nextInt();
+		
+		int temp1 = first, temp2 = second, ans = 0;
+		while(temp1 != 0) {
+			if(temp1 % 10 > temp2 % 10) {
+				ans = first;
 				break;
+			}else if(temp1 % 10 < temp2 % 10) {
+				ans = second;
+				break;
+			}else {
+				temp1 /= 10;
+				temp2 /= 10;
 			}
-			t1 /= 10;
-			t2 /= 10;
 		}
-		num = (num%10)*100 + (num%100-num%10) + (num/100);
-		System.out.print(num);
+		
+		int res = 0;
+		for(int i = 2; i >= 0; --i) {
+			res += (ans % 10) * Math.pow(10, i);
+			ans /= 10;
+		}
+		System.out.print(res);
 		scr.close();
 	}
 
