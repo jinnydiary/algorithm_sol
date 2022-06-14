0b647deba34cac0b71670226623197d263282211
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-14 17:23:11 +0900

    Update ACM 호텔.java
    이전 코드 업로드됨

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,31 +1,26 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner scr = new Scanner(System.in);
-		int t = scr.nextInt();
-		int h, w, n;
-		for(int i=0; i<t; i++) {
-			h = scr.nextInt();
-			w = scr.nextInt();
-			n = scr.nextInt();
-			System.out.println(findRoom(h, w, n));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 0; t < T; ++t) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int H = Integer.parseInt(st.nextToken()); //층 수
+			int W = Integer.parseInt(st.nextToken()); //방 수
+			int N = Integer.parseInt(st.nextToken()); //N번째 손님
+			
+			int r = (int)Math.ceil((double)N / (double)H); //방 번호
+			int f = (N%H == 0) ? H : N%H; //층 번호
+			System.out.println(f * 100 + r);
 		}
-		scr.close();
-	}
-	
-	private static int findRoom(int h, int w, int n) {
-		int d, position;
-		if(n%h == 0) {
-			d = n / h;
-			position = h;
-		}else {
-			d = (n / h) + 1;
-			position = n % h;
-		}
-		position = position * 100 + d;
-		return position;
 	}
 
 }
