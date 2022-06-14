4d6fc79ee9b6a1061150f1307afcbb3d2b497a86
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-14 11:16:40 +0900

    [Silver IV] Title: 스택, Time: 224 ms, Memory: 18260 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -8,26 +8,22 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<Integer>();
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = null;
+		int N = Integer.parseInt(br.readLine()); //명령의 수
 		
-		for(int i=0; i<N; ++i) {
-			st = new StringTokenizer(br.readLine());
+		Stack<Integer> stack = new Stack<Integer>();
+		for(int n = 0; n < N; ++n) {
+			st = new StringTokenizer(br.readLine(), " ");
 			String order = st.nextToken();
+			
 			switch(order) {
-				case "push":
-					int X = Integer.parseInt(st.nextToken());
-					stack.push(X);
-					break;
-				case "pop":
+				case "push": stack.push(Integer.parseInt(st.nextToken())); break;
+				case "pop": 
 					if(stack.isEmpty()) sb.append(-1 + "\n");
 					else sb.append(stack.pop() + "\n");
 					break;
-				case "size":
-					sb.append(stack.size() + "\n");
-					break;
+				case "size": sb.append(stack.size() + "\n"); break;
 				case "empty":
 					if(stack.isEmpty()) sb.append(1 + "\n");
 					else sb.append(0 + "\n");
@@ -39,8 +35,7 @@ public class Main {
 			}
 		}
 		
-		System.out.print(sb.substring(0, sb.length()-1));
-
+		System.out.print(sb);
 	}
 
 }
