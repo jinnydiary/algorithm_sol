5635c27d75055621a69c5d2e563e5e26c70834fd
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-15 10:50:26 +0900

    Update 설탕 배달.java
    이전 코드 업로드됨

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,25 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int result = findMin(N, new int[N+1]);
-		System.out.print(result == 0 ? -1 : result);
-
-	}
-
-	private static int findMin(int n, int[] num) {
-		for(int i = 3; i <= n; ++i) {
-			int temp = Integer.MAX_VALUE;
-			if( (i-3)%3 == 0 || (i-3)%5 == 0 || num[i-3] != 0) temp = Math.min(temp, num[i-3]+1);
-			if( (i-5>=0) && ((i-5)%5 == 0 || (i-5)%3 == 0 || num[i-5] != 0)) temp = Math.min(temp, num[i-5]+1);
-			num[i] = temp == Integer.MAX_VALUE ? 0 : temp;
+		int N = Integer.parseInt(br.readLine()); //설탕 무게
+		
+		int[] count = new int[N+1];
+		Arrays.fill(count, -1); //-1로 초기화
+		if(N < 5) count[3] = 1;
+		else count[3] = count[5] = 1;
+		
+		for(int n = 6; n <= N; ++n) {
+			if(count[n-3] != -1 && count[n-5] != -1) count[n] = Math.min(count[n-3], count[n-5]) + 1;
+			else if(count[n-3] != -1) count[n] = count[n-3] + 1;
+			else if(count[n-5] != -1) count[n] = count[n-5] + 1;
 		}
-		return num[n];
+		
+		System.out.print(count[N]);
 	}
 
 }
