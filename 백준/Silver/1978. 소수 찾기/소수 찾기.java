02bd032898d757a27f9c48178ddc2ab9f8043f7f
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-06-17 10:28:57 +0900

    Update 소수 찾기.java
    이전 코드 업로드됨

--- 백준/Silver/1978. 소수 찾기/소수 찾기.java
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -1,28 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner scr = new Scanner(System.in);
-		int n = scr.nextInt();
-		int count = 0;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		
-		for(int i=0; i<n; i++) {
-			int number = scr.nextInt();
-			boolean find = true;
-			for(int j=1; j<number; j++) {
-				if(number % j == 0 && j != 1) {
-					find = false;
-					break;
-				}
-			}
-			if(find == true && number != 1)
-				count++;
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int count = N;
+		for(int n = 0; n < N; ++n) {
+			int num = Integer.parseInt(st.nextToken());
+			if(findPrimeNumber(num)) --count;
 		}
 		
-		System.out.println(count);
-		scr.close();
-
+		System.out.print(count);
 	}
 
+	private static boolean findPrimeNumber(int num) { //소수 판별
+		if(num == 1) return true;
+		else {
+			int cnt = 1;
+			for(int i = 2; i <= num; ++i) {
+				if(num % i == 0) ++cnt;
+				if(cnt > 2) break;
+			}
+			
+			if(cnt > 2) return true;
+			else return false;
+		}
+	}
+	
 }
