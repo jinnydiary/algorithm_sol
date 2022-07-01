90734ca83382de533b5d0cf72200a5b177de03ba
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-07-01 21:42:51 +0900

    Update 피보나치 함수.java

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -2,30 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//백준 1003번 피보나치 함수(0930) - DP(동적계획법)
 public class Main {
 
+	static int[] zero, one;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		
 		int T = Integer.parseInt(br.readLine());
 		
-		for(int t = 1; t <= T; t++) {
+		for(int t = 0; t < T; ++t) {
 			int N = Integer.parseInt(br.readLine());
-			int[][] fibo = new int[N+2][2]; //각 숫자별로 0과 1의 호출 개수 담을 배열
-			fibo[0][0] = fibo[1][1] = 1;
-			fibo[0][1] = fibo[1][0] = 0;
-			
-			for(int n = 2; n<=N; n++) {
-				fibo[n][0] = fibo[n-1][0] + fibo[n-2][0];
-				fibo[n][1] = fibo[n-1][1] + fibo[n-2][1];
-			}
-			
-			sb.append(fibo[N][0] + " " + fibo[N][1] + "\n");
+			zero = new int[N+1];
+			one = new int[N+1];
+			if(N <= 0) zero[0] = 1;
+			else zero[0] = one[1] = 1;
+			fibo(N);
+			sb.append(zero[N] + " " + one[N] + "\n");
 		}
-		
 		System.out.print(sb);
-
+	}
+	
+	private static void fibo(int N) {
+		for(int n = 2; n <= N; ++n) {
+			zero[n] = zero[n-1] + zero[n-2];
+			one[n] = one[n-1] + one[n-2];
+		}
 	}
 
 }
