ea6c201ac6503a35370ce338bb9924c5a9478386
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-07-08 01:30:49 +0900

    Update 1로 만들기.java
    이전 코드 업로드됨

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,26 +1,25 @@
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		System.out.println(findMin(N, new int[N+1]));
-	}
-
-	private static int findMin(int n, int[] D) {
+		int[] count = new int[N+1];
+		if(N == 2) count[2] = 1;
+		else if(N > 2) count[2] = count[3] = 1;
 		
-		for(int i=2; i<=n; i++) {
-			int min = Integer.MAX_VALUE;				
-			if(i%3==0) min = Math.min(min, D[i/3]+1);
-			if(i%2==0) min = Math.min(min, D[i/2]+1);
-			if(i-1>=1) min = Math.min(min, D[i-1]+1);
-			D[i] = min;
+		for(int n = 4; n <= N; ++n) {
+			int min = Integer.MAX_VALUE;
+			if(n % 3 == 0) min = (min > count[n/3]+1) ? count[n/3]+1 : min; //3으로 나눠지는 경우
+			if(n % 2 == 0) min = (min > count[n/2]+1) ? count[n/2]+1 : min; //2로 나눠지는 경우
+			min = (min > count[n-1]+1) ? count[n-1]+1 : min; //1을 뺀 경우
+			count[n] = min;
 		}
-			
-		return D[n];
+		
+		System.out.print(count[N]);
 	}
-
+	
 }
