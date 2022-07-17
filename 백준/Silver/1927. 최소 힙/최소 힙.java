24f541f7a988be957c22d1eaee70de32c4c50859
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-07-17 10:17:57 +0900

    Update 최소 힙.java
    이전 코드 업로드됨

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -3,28 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
-//백준 1927번 최소 힙(1004)
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine()); //연산의 개수
 		
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(); //우선순위 큐(오름차순)
-		
-		for(int i=0; i<N; i++) {
+		PriorityQueue<Integer> queue = new PriorityQueue<>(); //우선순위 큐
+		for(int n = 0; n < N; ++n) {
 			int x = Integer.parseInt(br.readLine());
-			if(x == 0) { //0일 경우
-				if(pq.isEmpty()) sb.append(0 + "\n");
-				else sb.append(pq.poll() + "\n");
-			}else { //자연수일 경우
-				pq.offer(x);
-			}
+			if(x == 0) {
+				if(queue.size() == 0) sb.append(0 + "\n"); //빈 배열일 경우
+				else sb.append(queue.poll() + "\n");
+			}else queue.offer(x);
 		}
 		
 		System.out.print(sb);
-
 	}
-
 }
