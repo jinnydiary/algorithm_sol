d6873590969f74b6419a3b6267c465ffd22418ce
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-07-24 10:40:42 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//백준 2606번 바이러스(1004)
+public class Main {
+
+	static int N;
+	static int[][] adjMatrix;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		N = Integer.parseInt(br.readLine()); //컴퓨터 수
+		int connect = Integer.parseInt(br.readLine()); //직접 연결된 컴퓨터 쌍의 수
+		
+		adjMatrix = new int[N+1][N+1];
+		for(int i=0; i < connect; i++) {
+			st = new StringTokenizer(br.readLine());
+			int cp1 = Integer.parseInt(st.nextToken());
+			int cp2 = Integer.parseInt(st.nextToken());
+			adjMatrix[cp1][cp2] = adjMatrix[cp2][cp1] = 1; //직접 연결됨을 나타냄
+		}
+		
+		boolean[] visited = new boolean[N+1];
+		visited[1] = true;
+		dfs(1, visited);
+		
+		System.out.println(adjMatrix[1][0]);
+	}
+	
+	private static void dfs(int cnt, boolean[] visited) {
+		for(int i=2; i<=N; i++) {
+			if(adjMatrix[cnt][i] == 1 && !visited[i]) {
+				visited[i] = true;
+				++adjMatrix[1][0]; //1번 컴퓨터 직/간접적으로 연결되어 바이러스에 감열될 컴퓨터 수 증가
+				dfs(i, visited);
+			}
+		}
+	}
+
+}
