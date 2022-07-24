e7154242e4146ebfe10f42f4ebc1e35de0148f82
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-07-24 10:41:57 +0900

    Update 바이러스.java
    이전 코드 업로드됨

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,42 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-//백준 2606번 바이러스(1004)
 public class Main {
 
-	static int N;
-	static int[][] adjMatrix;
+	static int N, M, matrix[][];
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
 		N = Integer.parseInt(br.readLine()); //컴퓨터 수
-		int connect = Integer.parseInt(br.readLine()); //직접 연결된 컴퓨터 쌍의 수
+		M = Integer.parseInt(br.readLine()); //연결된 컴퓨터 쌍의 수
+		matrix = new int[N+1][N+1];
 		
-		adjMatrix = new int[N+1][N+1];
-		for(int i=0; i < connect; i++) {
-			st = new StringTokenizer(br.readLine());
-			int cp1 = Integer.parseInt(st.nextToken());
-			int cp2 = Integer.parseInt(st.nextToken());
-			adjMatrix[cp1][cp2] = adjMatrix[cp2][cp1] = 1; //직접 연결됨을 나타냄
+		StringTokenizer st = null;
+		for(int m = 1; m <= M; ++m) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int start = Integer.parseInt(st.nextToken()); //현재 웜 바이러스 걸린 컴퓨터
+			int next = Integer.parseInt(st.nextToken()); //웜 바이러스 걸릴 컴퓨터
+			matrix[start][next] = matrix[next][start] = 1; //컴퓨터 연결
 		}
 		
-		boolean[] visited = new boolean[N+1];
-		visited[1] = true;
-		dfs(1, visited);
-		
-		System.out.println(adjMatrix[1][0]);
+		System.out.print(countComputer());
+
 	}
 	
-	private static void dfs(int cnt, boolean[] visited) {
-		for(int i=2; i<=N; i++) {
-			if(adjMatrix[cnt][i] == 1 && !visited[i]) {
-				visited[i] = true;
-				++adjMatrix[1][0]; //1번 컴퓨터 직/간접적으로 연결되어 바이러스에 감열될 컴퓨터 수 증가
-				dfs(i, visited);
+	private static int countComputer() { //웜 바이러스 걸린 컴퓨터 세기(1번 제외)
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(1);
+		boolean[] virus = new boolean[N+1];
+		virus[1] = true;
+		
+		int count = 0;
+		while(!queue.isEmpty()) {
+			int now = queue.poll();
+			
+			for(int n = 1; n <= N; ++n) {
+				if(now != n && matrix[now][n] == 1 && !virus[n]) {
+					virus[n] = true;
+					queue.offer(n);
+					++count;
+				}
 			}
 		}
+		
+		return count;
 	}
-
+	
 }
