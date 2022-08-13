6a75e005b3f120fe3a3d1d24a2ee664cd79613fa
jinnydiary <62637811+jinnydiary@users.noreply.github.com>
2022-08-14 01:44:23 +0900

    Update 1， 2， 3 더하기.java
    이전 코드 업로드됨

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -2,37 +2,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-//백준 9095번 1,2,3 더하기
 public class Main {
 
-	static int result;
+	static int[] idea = new int[11];
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수
 		
-		for(int t = 0; t < T; t++) {
-			int n = Integer.parseInt(br.readLine()); //정수 n 입력
-			
-			result = 0;
-			perm(0, n);
-			
-			sb.append(result).append("\n");
+		idea[1] = 1;
+		for(int i = 2; i <= 10; ++i)
+			perm(0, i);
+		
+		for(int t = 0; t < T; ++t) {
+			int n = Integer.parseInt(br.readLine());
+			sb.append(idea[n] + "\n");
 		}
+		
 		System.out.print(sb);
-
 	}
 	
-	//방법 개수 구하기
-	private static void perm(int sum, int n) {
-		if(sum > n) return;
-		if(sum == n) {
-			result++;
+	private static void perm(int sum, int num) { //방법 찾기
+		if(sum > num) return; //구하려는 수보다 큰 경우
+		if(sum == num) { //구하려는 수인 경우
+			++idea[num]; //방법 1 증가
 			return;
 		}
-		
-		for(int i=1; i<=3; i++)
-			perm(sum+i, n);
-	}
 
+		for(int i = 1; i <= 3; ++i) //1~3 숫자 더하기
+			perm(sum+i, num);
+	}
+	
 }
